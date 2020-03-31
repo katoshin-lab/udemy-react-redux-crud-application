8097b768491c59fb8710cfe8c3c554b41eaec9fb
katoshin-lab <hatao0000000@outlook.com>
2020-03-31 14:28:35 +0900

    component
    class / functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("I am clicked.")}}></input>
-      </React.Fragment>
-    ) 
-  }
+// class component
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {console.log("I am clicked.")}}></input>
+//       </React.Fragment>
+//     ) 
+//   }
+// }
+
+// functional component
+const App = () => { 
+  return(
+  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
